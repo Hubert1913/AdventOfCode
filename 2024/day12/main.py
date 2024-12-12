635cf69b268a75be8cb443f33cddf269116f42f3
Hubert Nowak <H.D.Nowak@student.tudelft.nl>
2024-12-12 16:07:11 +0100

    Day 12 part 2

--- 2024/day12/main.py
+++ 2024/day12/main.py
@@ -7,23 +7,70 @@ def run_bfs(pos: (int, int), visited: set, data):
     que.append(pos)
     dirs = [1, 0, -1, 0, 1]
     per = 0
-    area = 1
-    visited.add(pos)
+    area = 0
+    # visited.add(pos)
+    sides = set()
 
     while len(que) != 0:
         cur = que.popleft()
+        if cur in visited:
+            continue
+        visited.add(cur)
+        area += 1
+
         for i in range(4):
             next_pos = (cur[0] + dirs[i], cur[1] + dirs[i+1])
             if 0 <= next_pos[0] < len(data) and 0 <= next_pos[1] < len(data[0]):
                 if data[next_pos[0]][next_pos[1]] == letter:
                     if next_pos not in visited:
                         que.append(next_pos)
-                        visited.add(next_pos)
-                        area += 1
+                        # visited.add(next_pos)
+                        # area += 1
                 else:
-                    per += 1
+                    # already_in = False
+                    # if dirs[i] == 0:
+                    #     if 0 <= cur[0] - 1:
+                    #         already_in = already_in or (data[cur[0]-1][cur[1]] == letter and data[next_pos[0]-1][next_pos[1]] != letter and (cur[0]-1, cur[1]) in visited)
+                    #     if cur[0] + 1 < len(data):
+                    #         already_in = already_in or (data[cur[0]+1][cur[1]] == letter and data[next_pos[0]+1][next_pos[1]] != letter and (cur[0]+1, cur[1]) in visited)
+                    # else:
+                    #     if 0 <= cur[1] - 1:
+                    #         already_in = already_in or (data[cur[0]][cur[1]-1] == letter and data[next_pos[0]][next_pos[1]-1] != letter and (cur[0], cur[1]-1) in visited)
+                    #         # if cur == (1, 5):
+                    #         #     print(data[cur[0]][cur[1]-1] == letter)
+                    #         #     print(data[next_pos[0]][next_pos[1]-1] != letter)
+                    #         #     print((next_pos[0], next_pos[1]-1))
+                    #         #     print((cur[0], cur[1]-1) in visited)
+                    #         #     print(already_in)
+                    #     if cur[1] + 1 < len(data[0]):
+                    #         already_in = already_in or (data[cur[0]][cur[1]+1] == letter and data[next_pos[0]][next_pos[1]+1] != letter and (cur[0], cur[1]+1) in visited)
+                    #         # if cur == (1, 5):
+                    #         #     print(already_in)
+                    # if not already_in:
+                    #     print(f"{cur}, ({dirs[i]},{dirs[i+1]})")
+                    #     # print(visited)
+                    #     per += 1
+                    sides.add((cur[0], cur[1], dirs[i], dirs[i+1]))
             else:
-                per += 1
+                # already_in = False
+                # if dirs[i] == 0:
+                #     if 0 <= cur[0] - 1:
+                #         already_in = already_in or (data[cur[0] - 1][cur[1]] == letter and (cur[0] - 1, cur[1]) in visited)
+                #     if cur[0] + 1 < len(data):
+                #         already_in = already_in or (data[cur[0] + 1][cur[1]] == letter and (cur[0] + 1, cur[1]) in visited)
+                # else:
+                #     if 0 <= cur[1] - 1:
+                #         already_in = already_in or (data[cur[0]][cur[1] - 1] == letter and (cur[0], cur[1] - 1) in visited)
+                #     if cur[1] + 1 < len(data[0]):
+                #         already_in = already_in or (data[cur[0]][cur[1] + 1] == letter and (cur[0], cur[1] + 1) in visited)
+                # if not already_in:
+                #     print(f"{cur}, ({dirs[i]},{dirs[i + 1]})")
+                #     per += 1
+                # per += 1
+                sides.add((cur[0], cur[1], dirs[i], dirs[i + 1]))
+
+    sides = {(x, y, dx, dy) for x, y, dx, dy in sides if (x-abs(dy), y-abs(dx), dx, dy) not in sides}
+    per = len(sides)
 
     return area, per
 
@@ -47,9 +94,6 @@ def main():
         for j in range(m):
             if (i, j) not in visited:
                 a, p = run_bfs((i, j), visited, data)
-                # print(data[i][j])
-                # print(a)
-                # print(p)
                 total += a * p
 
     print(total)
